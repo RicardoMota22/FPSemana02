0fc7e3f030d560ff3235c069cdd7195c7de7f262
RicardoMota22 <ricardomota.artist@gmail.com>
2024-11-15 13:57:05 +0000

    Second commit

--- char_intersection.py
+++ char_intersection.py
@@ -1,9 +1,19 @@
-name = str(input("first name?: "))
-name2 = str(input("second name?: "))
+"""name = str(input("first name?: "))
+name2 = str(input("second name?: "))"""
+
+
+
+
+name = set(input())
+name2 = set(input())
 
 #intersection
-set_1 = {name}
-set_2 = {name2}
-set_3 = set_1.intersection(set_2)
 
-print(set_3)
+
+set3 = (name.intersection(name2))  #u need to make the intersection a string
+
+set3 = "".join(set3)
+
+print(set3)
+
+
--- count_chars.py
+++ count_chars.py
@@ -1,12 +1,17 @@
 #FPSemana02
 
 #Count the amount of letters in every word in a phrase and show the amount next to the word
+
+user_dict = {}
+
 phrase =(str(input("Enter any phrase: ")))
 print(phrase)
 phrase = phrase.split()
-def b():
-    
-    for letter_c in phrase:
-        print(letter_c,":",len(letter_c))
 
-b()
+user_dict = {}
+
+    
+for letter_c in phrase:
+    user_dict[letter_c] = len(letter_c)
+        
+print(user_dict)
